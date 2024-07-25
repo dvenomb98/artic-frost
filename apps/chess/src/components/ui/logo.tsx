c9336d9ba49f65680276fd6a0ca92777284d7d86
Daniel Bílek <daniel.bilek@regiojet.com>
2024-07-25 12:45:41 +0200

    changed logo

--- apps/chess/src/components/ui/logo.tsx
+++ apps/chess/src/components/ui/logo.tsx
@@ -1,4 +1,4 @@
-import { ZapIcon } from "lucide-react";
+import PieceSVG from "@/chess/components/piece-svg";
 import Link from "next/link";
 import React, { FC } from "react";
 
@@ -6,12 +6,11 @@ import React, { FC } from "react";
 
 const Logo: FC = () => {
   return (
-    <Link href={"https://danielbilek.com"} className="flex items-center gap-2 rounded-md">
-      <ZapIcon className="w-5 h-5 fill-current" />
+    <Link href="/" className="flex items-center gap-2 rounded-md">
+      <PieceSVG className="w-5 h-5 fill-current" piece={"Q"} />
       <h1 className="inline-flex flex-col gap-0 leading-none font-medium">
         <span>DA</span>
-        <div><span className="font-bold">NIEL.</span>  <span className="text-xs text-muted-foreground">chess</span></div>
-      
+        <span className="font-bold">NIEL.</span>
       </h1>
     </Link>
   );
