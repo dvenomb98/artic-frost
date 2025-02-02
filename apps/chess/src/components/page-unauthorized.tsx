6de526b6fc2f3cc827a9f963b9c8d67178a4c639
dvenomb98 <danielbilek98@seznam.cz>
2025-02-02 10:09:03 +0100

    styling

--- apps/chess/src/components/page-unauthorized.tsx
+++ apps/chess/src/components/page-unauthorized.tsx
@@ -10,14 +10,14 @@ function PageUnauthorized() {
   }
 
   return (
-    <div className="h-full w-full bg-muted grid place-content-center">
-      <div className="flex items-center justify-center flex-col">
+    <div className="h-full w-full bg-muted/20 grid place-content-center">
+      <div className="flex items-center justify-center flex-col gap-4 border rounded-md p-10 bg-card">
         <LockIcon size={100} className="text-muted-foreground" />
-        <p className="max-w-[250px] text-center">
+        <p className="max-w-[250px] text-center h4">
           This feature is only available for <strong>non-anonymous</strong>{" "}
           users.
         </p>
-        <Button className="mt-4" onClick={not_available}>
+        <Button onClick={not_available}>
           Convert your account
         </Button>
       </div>
