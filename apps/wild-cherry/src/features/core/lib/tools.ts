8f0d23542a32329ae2cbd11b3f84e4529f1931e9
dvenomb98 <danielbilek98@seznam.cz>
2025-02-22 15:16:08 +0100

    iamge data solution

--- apps/wild-cherry/src/features/core/lib/tools.ts
+++ apps/wild-cherry/src/features/core/lib/tools.ts
@@ -1,5 +1,4 @@
 import { Minus, Paintbrush, Pencil } from "lucide-react";
-
 import { Point } from "./types";
 
 type ToolHandler = {
@@ -66,20 +65,27 @@ const TOOLS = {
     handler: {
       onMouseDown: (ctx, point) => {
         drawInitShape(ctx, point);
-        
-        const { x, y } = point;
-        ctx.beginPath();
-        ctx.moveTo(x, y);
+        createTemp(ctx, point);
       },
       onMouseMove: (ctx, point) => {
+        const { startPoint, savedImageData } = getTemp();
+        if (!savedImageData) return;
         const { x, y } = point;
-      },
-      onMouseUp: (ctx, point) => {
-        const { x, y } = point;
+
+        ctx.putImageData(savedImageData, 0, 0);
+        ctx.beginPath();
+        ctx.moveTo(startPoint.x, startPoint.y);
         ctx.lineTo(x, y);
         ctx.stroke();
       },
-      onMouseLeave: () => {},
+      onMouseUp: (ctx, point) => {
+        // reset temp (optional)
+        clearTemp();
+      },
+      onMouseLeave: (ctx) => {
+        // reset temp (optional)
+        clearTemp();
+      },
     } satisfies ToolHandler,
     shape_options: {
       round: [2, 4, 6],
@@ -92,10 +98,49 @@ type ToolId = (typeof TOOLS)[keyof typeof TOOLS]["id"];
 
 export { type ExtractedLineCap, type ToolHandler, type ToolId, TOOLS };
 
+/**
+ * Drawing helpers
+ */
+
 function drawInitShape(ctx: CanvasRenderingContext2D, point: Point) {
   const { x, y } = point;
   ctx.beginPath();
   ctx.moveTo(x, y);
   ctx.lineTo(x, y);
   ctx.stroke();
+}
+
+/**
+ * DRAWING STATE
+ * used to track drawing state for more complex shapes. dont mutate in directly, only via functions.
+ */
+
+type Temp = {
+  startPoint: Point;
+  savedImageData: ImageData | null;
+};
+
+let __startPoint__: Point = { x: 0, y: 0 };
+let __savedImageData__: ImageData | null = null;
+
+function createTemp(ctx: CanvasRenderingContext2D, point: Point) {
+  __startPoint__ = { ...point };
+  __savedImageData__ = ctx.getImageData(
+    0,
+    0,
+    ctx.canvas.width,
+    ctx.canvas.height,
+  );
+}
+
+function getTemp(): Temp {
+  return {
+    startPoint: __startPoint__,
+    savedImageData: __savedImageData__,
+  };
+}
+
+function clearTemp(): void {
+  __startPoint__ = { x: 0, y: 0 };
+  __savedImageData__ = null;
 }
