d3df94c6f599199e179fea26f2a754b38387101d
dvenomb98 <danielbilek98@seznam.cz>
2025-08-26 22:36:11 +0200

    web assembly webpack

--- apps/chess/next.config.mjs
+++ apps/chess/next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@artic-frost/ui", "@artic-frost/chess-lite"],
-  serverExternalPackages: ["wasm-chess"]
+  /**
+   * @param {import('webpack').Configuration} config
+   * @param {import('next/dist/server/config-shared').WebpackConfigContext} context
+   * @returns {import('webpack').Configuration}
+   */
+  webpack: config => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
