85314e578470c15517be564884ee9fb33043e68a
dvenomb98 <danielbilek98@seznam.cz>
2025-10-08 22:53:52 +0200

    it didnt fix shit, fuck you vercel

--- apps/chess/next.config.mjs
+++ apps/chess/next.config.mjs
@@ -1,5 +1,3 @@
-import CopyPlugin from "copy-webpack-plugin";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@artic-frost/ui", "@artic-frost/form"],
@@ -8,32 +6,11 @@ const nextConfig = {
    * @param {import('next/dist/server/config-shared').WebpackConfigContext} context
    * @returns {import('webpack').Configuration}
    */
-  webpack: (config, {isServer, dev}) => {
+  webpack: (config) => {
     config.experiments = {
       ...config.experiments,
       asyncWebAssembly: true,
     };
-
-    if (!dev && isServer) {
-      const patterns = [];
-
-      const destinations = [
-        "../static/wasm/[name][ext]", // -> .next/static/wasm
-        "./static/wasm/[name][ext]", // -> .next/server/static/wasm
-        ".", // -> .next/server/chunks (for some reason this is necessary)
-      ];
-      for (const dest of destinations) {
-        patterns.push({
-          context: ".next/server/chunks",
-          from: ".",
-          to: dest,
-          filter: resourcePath => resourcePath.endsWith(".wasm"),
-          noErrorOnMissing: true,
-        });
-      }
-
-      config.plugins.push(new CopyPlugin({patterns}));
-    }
 
     return config;
   },
